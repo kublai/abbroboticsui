d519cdd2994f2cc3ef6e8e6d9a089c73f998a182
Kublai Gomez <kublai@bytebox.es>
2023-12-16 11:37:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import logo from './ABB_logo.svg';
+import {useEffect, useState, useRef} from 'react';
+import './App.css';
+import DisplaySlot from './DisplaySlot';
+
+
+
+export default function App() {
+  const [partNumber, setPartNumber] = useState(1);
+  const [data, setData] = useState('');
+  const [slots, setSlots] = useState([]);
+  const wsconn = useRef(null); //store the websocket connection in a reference
+
+  //onMount
+  useEffect(()=>{
+    if(wsconn.ws !== undefined){  //only one connection per user allowed
+      return;
+    };
+    wsconn.ws = new WebSocket("wss://localhost:3000");
+    wsconn.ws.onmessage = function(event){
+      const newData = JSON.parse(event.data);
+      if (newData.features !== undefined ){
+        const featureSlots = newData.features.map( (item,index) => {
+          let slotClassMain = "grid-item ";
+          let slotClassHeight = "grid-overflow ";
+          if (index === 0){
+            slotClassMain += "grid-slot-big";
+            slotClassHeight += "double-height";
+          } else if (index === 1){
+            slotClassMain += "grid-slot-medium";
+            slotClassHeight += "double-height";
+          } else {
+            slotClassMain += "grid-slot-standard";
+            slotClassHeight += "single-height";
+          }
+          return <DisplaySlot key={index} classNameMain={slotClassMain} classNameSlot={slotClassHeight} featureData={item}  ></DisplaySlot>
+        });
+        //console.log(featureSlots);
+        setData(event.data);
+        setSlots(featureSlots);
+      } else { //new part has been selected, clean the screen and show a message
+        if (newData.part > 2){
+          setSlots(<div className="loader">Only parts 1 and 2 are available in this Demo</div>);
+        }else {
+          const message = `Please wait for Part ${newData.part} data...`;
+          setSlots(<div className="loader">{message}</div>);
+        }
+      }
+  }
+  },[]);
+
+  /**
+   * change the part number value on onchange
+   * 
+   */
+  function changePartNumber(e){
+    setPartNumber(e.target.value);
+  }
+
+  /**
+   * Send a ws message with the part selected
+   */
+  function sendPartNumber(e){
+   const res = wsconn.ws.send(partNumber);
+  }
+
+  return (
+    <div className="app">
+      <header className="appHeader">
+        <img src={logo} className="App-logo" alt="logo" height="32px" />
+        <div style={{"marginTop":"12px"}}>
+          <label htmlFor="partNumber">Part Number</label>
+          <input type="text" name="partNumber" id="partNumber" className="inputBox" value={partNumber} onChange={changePartNumber}/>
+          <button type="button" name="btnPartNumber" className="btnDefault" onClick={sendPartNumber}>Get Data</button>
+        </div>
+      </header>
+      <div className="grid-container">
+        {slots}
+      </div>
+    </div>
+  );
+}
+
+
---
+++ src/DisplaySlot.jsx
@@ -0,0 +1,153 @@
+
+import './App.css';
+
+export default function DisplaySlot({classNameMain, classNameSlot, featureData}) {
+  // calculate the header status background and icon for the component
+  const [ slotHeaderBg, slotHeaderIcon ] = getHeaderClasses(featureData.controls);
+
+  //console.log(featureData);
+  let controlsList1 = [];
+  let controlsList2 = [];
+  let bigSlotFlag = (classNameMain.indexOf("grid-slot-big") > -1);
+  if (bigSlotFlag){ //for big-slots divide the data in two groups
+    const totalElements = featureData.controls.length;
+    const group1 = featureData.controls.slice(0,totalElements/2); //splice remove the elements
+    const group2 = featureData.controls.slice(totalElements/2);
+    controlsList1 = generateRows(group1);
+    controlsList2 = generateRows(group2);
+ } else {
+    controlsList1 = generateRows(featureData.controls);
+  };
+  
+  if (bigSlotFlag){
+    return (
+      <div className={classNameMain}>
+        <><div className="slot-header-container">
+          <div className={`slot-header `+ slotHeaderBg }>
+            <div><span className="material-symbols-outlined white">circle</span></div>
+            <div className="white"> {featureData.name} </div>
+            <div><span className="material-symbols-outlined white">{slotHeaderIcon}</span> </div>
+          </div>
+        </div>
+        <div className={`display-flex `+ classNameSlot}>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ "width": "65px", "height": "18px"}}>Control</th>
+                <th style={{ "width": "65px", "height": "18px" }}>Dev</th>
+                <th style={{ "width": "108px", "height":  "18px" }} colSpan={2}>Dev Out Tol</th>
+              </tr>
+            </thead>
+            <tbody>
+              {controlsList1}
+            </tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ "width": "65px", "height": "18px" }}>Control</th>
+                <th style={{ "width": "65px", "height": "18px" }}>Dev</th>
+                <th style={{ "width": "108px", "height": "18px" }} colSpan={2}>Dev Out Tol</th>
+              </tr>
+            </thead>
+            <tbody>
+              {controlsList2}
+            </tbody>
+          </table>
+        </div></>       
+      </div>
+    );
+  }else{
+    return (
+      <div className={classNameMain}>
+        <><div className="slot-header-container">
+          <div className={`slot-header `+ slotHeaderBg }>
+            <div><span className="material-symbols-outlined white">circle</span></div>
+            <div className="white"> {featureData.name} </div>
+            <div><span className="material-symbols-outlined white">{slotHeaderIcon}</span> </div>
+          </div>
+        </div>
+        <div className={classNameSlot}>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ "width": "65px", "height": "18px" }}>Control</th>
+                <th style={{ "width": "65px", "height": "18px" }}>Dev</th>
+                <th style={{ "width": "108px", "height": "18px" }} colSpan={2}>Dev Out Tol</th>
+              </tr>
+            </thead>
+            <tbody>
+              {controlsList1}
+            </tbody>
+          </table>
+        </div></>       
+      </div>
+    );
+  }
+
+}
+
+/**
+ * Generates data rows for the tables including icon and color
+ * @param {Array} group 
+ * @returns 
+ */
+function generateRows(group){
+  //console.log(group);
+  return group.map( (item,index) => {
+    const control = item.control;
+    const measurement = item.measurement;
+    const deviation = item.deviation;
+    const tolerance = item.tolerance;
+    let dev = measurement * (1 + deviation); //algebraic sum,  works for positive and negative deviations
+    const measWithTol = measurement + tolerance;
+    let dot = measWithTol - dev;
+    dev = Math.round(dev*100)/100;
+    dot = Math.round(dot*100)/100;
+    let status = "cancel" //error icon
+    let statusColor = "red";
+    if ((Math.abs(dot)/dev) <= 0.10){
+      status = "check_circle" //ok icon
+      statusColor = "green";
+    }else if((Math.abs(dot)/dev) <= 0.30){
+      status = "error" //warning icon
+      statusColor = "yellow";
+    }
+    return <tr key={index}>
+      <td className="textLeft" style={{"width":"62px",  "height": "35px"}}>{control}</td>
+      <td className="textRight" style={{"width":"62px",  "height": "35px"}}>{dev}</td>
+      <td className="textRight" style={{"width":"62px",  "height": "35px"}}>{dot}</td>
+      <td className="textCenter" style={{"width":"42px",  "height": "35px"}}><span className={"material-symbols-outlined " + statusColor}>{status}</span></td> 
+    </tr>
+  });
+}
+
+/**
+ * calculates the general status background and icon for the component
+ * @param {Array} group 
+ */
+function getHeaderClasses(group){
+  let flagWarningLevel = false;
+  let flagErrorLevel = false;
+  group.forEach(item => {
+    let dev = item.measurement * (1 + item.deviation);
+    let dot = item.measurement + item.tolerance - dev;
+    dev = Math.round(dev*100)/100;
+    dot = Math.round(dot*100)/100;
+    if (flagErrorLevel === false && flagWarningLevel === false && (Math.abs(dot)/dev) > 0.10 && (Math.abs(dot)/dev) <= 0.30){
+      flagWarningLevel = true;
+      return;
+    }
+    if(flagErrorLevel === false  && (Math.abs(dot)/dev) > 0.30){
+      flagErrorLevel = true;
+      return;
+    }
+  });
+  if (flagErrorLevel === true) {
+    return ["bg-red", "cancel"];
+  }
+  if (flagWarningLevel === true) {
+    return ["bg-yellow", "error"]; //warning icon
+  }
+  return ["bg-green", "check_circle"];
+}
